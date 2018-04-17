eb30d7710837276ce45ebda3ef02e3f95ea6f6d1
footlessbird <arslongavitabrevis0818@gmail.com>
2018-04-17 14:57:11 -0700

    User's profile photo on register screen done

--- app/src/main/java/com/melontalk/footlessbird/melontalk/RegisterActivity.java
+++ app/src/main/java/com/melontalk/footlessbird/melontalk/RegisterActivity.java
@@ -1,13 +1,17 @@
 package com.melontalk.footlessbird.melontalk;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,16 +19,20 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
 import com.melontalk.footlessbird.melontalk.model.UserModel;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final int PICK_FROM_ALBUM = 10;
     private EditText email;
     private EditText name;
     private EditText password;
     private Button register;
     private String splash_background;
-
+    private ImageView profile;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,16 @@ public class RegisterActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
         }
+        profile = findViewById(R.id.registerActivity_imageView_profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_FROM_ALBUM);
+            }
+        });
+
 
         email = findViewById(R.id.registerActivity_editText_email);
         name = findViewById(R.id.registerActivity_editText_name);
@@ -55,15 +73,35 @@ public class RegisterActivity extends AppCompatActivity {
                         .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                UserModel userModel = new UserModel();
+                                final String userId = task.getResult().getUser().getUid();
 
-                                userModel.userName = name.getText().toString();
+                                FirebaseStorage.getInstance().getReference().child("userImages").child(userId).putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                        String imageUrl = task.getResult().getDownloadUrl().toString();
 
-                                String userId = task.getResult().getUser().getUid();
-                                FirebaseDatabase.getInstance().getReference().child("users").child(userId).setValue(userModel);
+                                        UserModel userModel = new UserModel();
+                                        userModel.userName = name.getText().toString();
+                                        userModel.profileImageUrl = imageUrl;
+                                        FirebaseDatabase.getInstance().getReference().child("users").child(userId).setValue(userModel);
+
+                                    }
+                                });
+
+
                             }
                         });
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_FROM_ALBUM && resultCode == RESULT_OK)
+            profile.setImageURI(data.getData());    //  changes center views
+            imageUri = data.getData();
     }
 }
--- app/src/main/java/com/melontalk/footlessbird/melontalk/model/UserModel.java
+++ app/src/main/java/com/melontalk/footlessbird/melontalk/model/UserModel.java
@@ -2,5 +2,5 @@ package com.melontalk.footlessbird.melontalk.model;
 
 public class UserModel {
     public String userName;
-
+    public String profileImageUrl;
 }

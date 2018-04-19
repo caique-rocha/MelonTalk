67a1d16a0b68122a34372bb1929cc353aa9c6490
footlessbird <arslongavitabrevis0818@gmail.com>
2018-04-19 14:16:17 -0700

    Working on PeopleFragment
    Glide (image library) added

--- app/src/main/java/com/melontalk/footlessbird/melontalk/MainActivity.java
+++ app/src/main/java/com/melontalk/footlessbird/melontalk/MainActivity.java
@@ -3,11 +3,16 @@ package com.melontalk.footlessbird.melontalk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.melontalk.footlessbird.melontalk.fragment.PeopleFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction().replace(R.id.mainActivity_frameLayout, new PeopleFragment()).commit();
+
     }
 }
---
+++ app/src/main/java/com/melontalk/footlessbird/melontalk/fragment/PeopleFragment.java
@@ -0,0 +1,96 @@
+package com.melontalk.footlessbird.melontalk.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.melontalk.footlessbird.melontalk.R;
+import com.melontalk.footlessbird.melontalk.model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_people, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.peopleFragment_recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        recyclerView.setAdapter(new PeopleFragmentRecyclerViewAdapter());
+
+        return view;
+    }
+
+    class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        List<UserModel> userModels;
+
+        public PeopleFragmentRecyclerViewAdapter() {
+            userModels = new ArrayList<>();
+            FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    userModels.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        userModels.add(snapshot.getValue(UserModel.class));
+                    }
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friends, parent, false);
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            Glide.with(holder.itemView.getContext())
+                    .load(userModels.get(position).profileImageUrl)
+                    .apply(new RequestOptions().circleCrop())
+                    .into(((CustomViewHolder) holder).imageView);
+            ((CustomViewHolder) holder).textView.setText(userModels.get(position).userName);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return userModels.size();
+        }
+
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+            public ImageView imageView;
+            public TextView textView;
+
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = view.findViewById(R.id.itemFriends_imageView);
+                textView = view.findViewById(R.id.itemFriends_textView);
+            }
+        }
+    }
+}

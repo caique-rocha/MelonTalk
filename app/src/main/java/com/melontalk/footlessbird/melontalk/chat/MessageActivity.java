1f5489b4fe27e013010448cd94969beafed09610
footlessbird <arslongavitabrevis0818@gmail.com>
2018-04-20 14:15:07 -0700

    Working on sending message

--- app/src/main/java/com/melontalk/footlessbird/melontalk/chat/MessageActivity.java
+++ app/src/main/java/com/melontalk/footlessbird/melontalk/chat/MessageActivity.java
@@ -7,7 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.melontalk.footlessbird.melontalk.R;
 import com.melontalk.footlessbird.melontalk.model.ChatModel;
 
@@ -16,11 +19,15 @@ public class MessageActivity extends AppCompatActivity {
     private String destinationUid;
     private Button button;
     private EditText editText;
+
+    private String uId;
+    private String chatRoomUid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
-
+        uId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         destinationUid = getIntent().getStringExtra("destinationUid");
         button = findViewById(R.id.messageActivity_button);
         editText = findViewById(R.id.messageActivity_editText);
@@ -29,10 +36,38 @@ public class MessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ChatModel chatModel = new ChatModel();
-                chatModel.uId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                chatModel.destinationUid = destinationUid;
+                chatModel.users.put(uId, true);
+                chatModel.users.put(destinationUid, true);
 
-                FirebaseDatabase.getInstance().getReference().child("chatRooms").push().setValue(chatModel);
+                if (chatRoomUid == null) {
+                    FirebaseDatabase.getInstance().getReference().child("chatRooms").push().setValue(chatModel);
+
+                }else{
+                    ChatModel.Comment comment = new ChatModel.Comment();
+                    comment.uId = uId;
+                    comment.message = editText.getText().toString();
+                    FirebaseDatabase.getInstance().getReference().child("chatRooms").child("comments").push().setValue(comment);
+                }
+            }
+        });
+        checkChatRoom();
+    }
+
+    void checkChatRoom() {
+        FirebaseDatabase.getInstance().getReference().child("chatRooms").orderByChild("users/" + uId).equalTo(true).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot item : dataSnapshot.getChildren()) {
+                    ChatModel chatModel = item.getValue(ChatModel.class);
+                    if (chatModel.users.containsKey(destinationUid)) {
+                        chatRoomUid = item.getKey();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
--- app/src/main/java/com/melontalk/footlessbird/melontalk/model/ChatModel.java
+++ app/src/main/java/com/melontalk/footlessbird/melontalk/model/ChatModel.java
@@ -1,7 +1,17 @@
 package com.melontalk.footlessbird.melontalk.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChatModel {
-    public String uId;
-    public String destinationUid;
+
+    public Map<String, Boolean> users = new HashMap<>();    //  users in chatting room
+    public Map<String, Comment> comments = new HashMap<>();
+
+    public static class Comment {
+
+       public String uId;
+       public String message;
+    }
 
 }

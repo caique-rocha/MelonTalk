f6407cd5a2782334523c21edf39315fe833e81eb
footlessbird <arslongavitabrevis0818@gmail.com>
2018-04-21 18:28:41 -0700

    Chat bubble applied

--- app/src/main/java/com/melontalk/footlessbird/melontalk/chat/MessageActivity.java
+++ app/src/main/java/com/melontalk/footlessbird/melontalk/chat/MessageActivity.java
@@ -1,6 +1,5 @@
 package com.melontalk.footlessbird.melontalk.chat;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,8 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.melontalk.footlessbird.melontalk.R;
 import com.melontalk.footlessbird.melontalk.model.ChatModel;
+import com.melontalk.footlessbird.melontalk.model.UserModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,18 +49,16 @@ public class MessageActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.messageActivity_button);
         editText = (EditText) findViewById(R.id.messageActivity_editText);
 
-        recyclerView = (RecyclerView)findViewById(R.id.messageActivity_recyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.messageActivity_recyclerView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ChatModel chatModel = new ChatModel();
-                chatModel.users.put(uId,true);
-                chatModel.users.put(destinationUid,true);
+                chatModel.users.put(uId, true);
+                chatModel.users.put(destinationUid, true);
 
 
-
-
-                if(chatRoomUid == null){
+                if (chatRoomUid == null) {
                     button.setEnabled(false);
                     FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(chatModel).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
@@ -65,7 +67,7 @@ public class MessageActivity extends AppCompatActivity {
                         }
                     });
 
-                }else {
+                } else {
 
                     ChatModel.Comment comment = new ChatModel.Comment();
                     comment.uId = uId;
@@ -81,14 +83,14 @@ public class MessageActivity extends AppCompatActivity {
 
     }
 
-    void  checkChatRoom(){
+    void checkChatRoom() {
 
-        FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/"+uId).equalTo(true).addListenerForSingleValueEvent(new ValueEventListener() {
+        FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/" + uId).equalTo(true).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot item : dataSnapshot.getChildren()){
-                    ChatModel  chatModel = item.getValue(ChatModel.class);
-                    if(chatModel.users.containsKey(destinationUid)){
+                for (DataSnapshot item : dataSnapshot.getChildren()) {
+                    ChatModel chatModel = item.getValue(ChatModel.class);
+                    if (chatModel.users.containsKey(destinationUid)) {
                         chatRoomUid = item.getKey();
                         button.setEnabled(true);
                         recyclerView.setLayoutManager(new LinearLayoutManager(MessageActivity.this));
@@ -103,19 +105,39 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
     }
-    class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
 
         List<ChatModel.Comment> comments;
+        UserModel userModel;
+
         public RecyclerViewAdapter() {
             comments = new ArrayList<>();
 
+            FirebaseDatabase.getInstance().getReference().child("users").child(destinationUid).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    userModel = dataSnapshot.getValue(UserModel.class);
+                    getMessageList();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+
+        void getMessageList() {
             FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid).child("comments").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     comments.clear();
 
-                    for(DataSnapshot item : dataSnapshot.getChildren()){
+                    for (DataSnapshot item : dataSnapshot.getChildren()) {
                         comments.add(item.getValue(ChatModel.Comment.class));
                     }
 
@@ -129,13 +151,11 @@ public class MessageActivity extends AppCompatActivity {
 
                 }
             });
-
-
         }
 
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_messages,parent,false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_messages, parent, false);
 
 
             return new MessageViewHolder(view);
@@ -143,9 +163,24 @@ public class MessageActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            MessageViewHolder messageViewHolder = ((MessageViewHolder) holder);
+            if (comments.get(position).uId.equals(uId)) {
+                messageViewHolder.textView_message.setText(comments.get(position).message);
+                messageViewHolder.textView_message.setBackgroundResource(R.drawable.rightbubble);
+                messageViewHolder.linearLayout_destination.setVisibility(View.INVISIBLE);
 
+            } else {
+                Glide.with(holder.itemView.getContext())
+                        .load(userModel.profileImageUrl)
+                        .apply(new RequestOptions().circleCrop())
+                        .into(messageViewHolder.imageView_profile);
+                messageViewHolder.textView_name.setText(userModel.userName);
+                messageViewHolder.linearLayout_destination.setVisibility(View.VISIBLE);
+                messageViewHolder.textView_message.setBackgroundResource(R.drawable.leftbubble);
+                messageViewHolder.textView_message.setText(comments.get(position).message);
+                messageViewHolder.textView_message.setTextSize(25);
+            }
 
-            ((MessageViewHolder)holder).textView_message.setText(comments.get(position).message);
         }
 
         @Override
@@ -155,10 +190,16 @@ public class MessageActivity extends AppCompatActivity {
 
         private class MessageViewHolder extends RecyclerView.ViewHolder {
             public TextView textView_message;
+            public TextView textView_name;
+            public ImageView imageView_profile;
+            public LinearLayout linearLayout_destination;
 
             public MessageViewHolder(View view) {
                 super(view);
-                textView_message = (TextView) view.findViewById(R.id.itemMessages_textView);
+                textView_message = (TextView) view.findViewById(R.id.itemMessages_textView_message);
+                textView_name = view.findViewById(R.id.itemMessages_textView_name);
+                imageView_profile = view.findViewById(R.id.itemMessages_imageView_profile);
+                linearLayout_destination = view.findViewById(R.id.itemMessages_linearLayout_destination);
             }
         }
     }
